718a607431005e173b940938a8be1b9977c6416d
NeiDeMing <xuan136371773@gmail.com>
2020-09-02 16:50:07 +0800

    test(fix test)

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../App';
+import BasicLayout from '../layouts/BasicLayout';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello/i);
+  const { getByText } = render(<BasicLayout />);
+  const linkElement = getByText(/BasicLayout/i);
   expect(linkElement).toBeInTheDocument();
 });
